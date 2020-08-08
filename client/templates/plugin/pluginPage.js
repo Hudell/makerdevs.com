9b52cc29b420b5c4e5a96f1cae7a7c775d373af8
Hudell <brian@hudell.com>
2020-08-08 06:00:50 -0300

    404 page, toastr, plugin details

--- client/templates/plugin/pluginPage.js
+++ client/templates/plugin/pluginPage.js
@@ -1,4 +1,9 @@
+import { Meteor } from 'meteor/meteor';
 import { FlowRouter } from 'meteor/kadira:flow-router';
+import { ReactiveVar } from 'meteor/reactive-var';
+import { Session } from 'meteor/session';
+
+import toastr from 'toastr';
 
 import Plugins from '../../../models/Plugins';
 import './pluginPage.html';
@@ -6,20 +11,29 @@ import './pluginPage.html';
 
 Template.pluginPage.helpers({
   plugin() {
-    return Template.instance().plugin;
+    return Template.instance().plugin.get();
   },
   isLoaded() {
-    return Template.instance().isLoaded;
+    return Template.instance().isLoaded.get();
   }
 });
 
-Template.pluginPage.onCreated(() => {
+Template.pluginPage.onCreated(function() {
   const pluginId = FlowRouter.getParam('pluginId');
 
   this.plugin = new ReactiveVar(false);
   this.isLoaded = new ReactiveVar(false);
 
+  Meteor.call('plugin/details', pluginId, (err, data) => {
+    this.isLoaded.set(true);
 
-  // return Plugins.findOneById(pluginId);
+    if (err) {
+      toastr.error("Failed to load plugin data.");
+      console.log(err);
+      return;
+    }
 
+    Session.set('pageTitle', `Plugin Details - ${ data.name }`);
+    this.plugin.set(data);
+  });
 });
--- server/methods/plugin.ts
+++ server/methods/plugin.ts
@@ -1,2 +1,13 @@
 import { Meteor } from 'meteor/meteor';
+import { check } from 'meteor/check'
 
+import Plugins from '../../models/Plugins';
+
+Meteor.methods({
+  'plugin/details'(pluginId) {
+    check(pluginId, String);
+
+    const plugin = Plugins.findOneById(pluginId);
+    return plugin;
+  }
+});
