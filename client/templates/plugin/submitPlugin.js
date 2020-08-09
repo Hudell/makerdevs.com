016588601a99606f39cae331fe5ac5d4dfdab550
Hudell <brian@hudell.com>
2020-08-09 02:14:20 -0300

    submit plugin

--- client/templates/plugin/submitPlugin.js
+++ client/templates/plugin/submitPlugin.js
@@ -1,1 +1,32 @@
 import './submitPlugin.html';
+
+Template.submitPlugin.helpers({
+
+});
+
+Template.submitPlugin.events({
+  'click .submit'(e, instance) {
+    e.preventDefault();
+
+    const mv = $('#platform_mv').is(':checked');
+    const mz = $('#platform_mz').is(':checked');
+
+    const name = $('#pluginName').val();
+    const description = $('#pluginDescription').val();
+
+    const public = $('#pluginPublic').is(':checked');
+    const versionName = $('#pluginVersion').val();
+    const externalLink = $('#pluginLink').val();
+
+    console.log({
+      mv,
+      mz,
+      name,
+      description,
+      public,
+    })
+
+
+
+  },
+});
