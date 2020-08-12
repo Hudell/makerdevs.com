38b4bc963a2cc536d0fef569fe7f064473ef311a
Ramiro Rojo <ramiro.rojo.cretta@gmail.com>
2020-08-12 15:38:14 -0300

    Add view for password reset

--- server/startup.ts
+++ server/startup.ts
@@ -1,9 +1,13 @@
 import Platforms from '../models/Platforms';
 import { refreshLoginServices } from './functions/refreshLoginServices';
+import { Accounts } from 'meteor/accounts-base';
 
 // import './importOldData';
 
 Meteor.startup(() => {
+  Accounts.urls.resetPassword = function(token) {
+    return Meteor.absoluteUrl('reset-password/' + token);
+  };
   // Create the default platforms
   Platforms.ensurePlatform('mv', 'RPG Maker MV');
   Platforms.ensurePlatform('mz', 'RPG Maker MZ');
