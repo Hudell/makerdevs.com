db97745ccbafe76f6e19e5f42f9835ab0148e1f7
Ramiro Rojo <ramiro.rojo.cretta@gmail.com>
2020-08-13 16:33:09 -0300

    Add platform icons on plugin card

--- client/templates/plugin/pluginCard.js
+++ client/templates/plugin/pluginCard.js
@@ -1,8 +1,20 @@
 import './pluginCard.html';
 
+const PLATFORM_NAMES = {
+  mv: 'RPG Maker MV',
+  mz: 'RPG Maker MZ'
+};
+
 Template.pluginCard.helpers({
   displayName() {
     const name = this.name;
     return name;
+  },
+  icons() {
+    const lastVersion = this.versions[this.versions.length - 1];
+    return lastVersion.platforms.map(i => ({
+      name: PLATFORM_NAMES[i] || 'Unknown Platform',
+      src: `/icons/platforms/${i}.png`
+    }));
   }
 });
