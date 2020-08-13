7087ce1b1ca40a776a5f66ae33c74f1504061e8d
Hudell <brian@hudell.com>
2020-08-12 23:03:57 -0300

    Small improvements

--- client/templates/masterList/masterListLite.js
+++ client/templates/masterList/masterListLite.js
@@ -22,16 +22,16 @@ const getModel = () => {
 
 const getAllPlugins = (symbol = '') => {
   const model = getModel();
-  
+
   return model.findAllBySymbol(symbol);
 };
 
+const possibleHeaders = ['#', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 Template.masterListLite.helpers({
   headers() {
     const model = getModel();
-    const possibleHeaders = ['#', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
     const headers = [];
-    
+
     for (const symbol of possibleHeaders) {
       if (model.hasAnyPluginWithSymbol(symbol)) {
         headers.push(symbol);
@@ -47,5 +47,9 @@ Template.masterListLite.helpers({
 
   plugins() {
     return getAllPlugins();
+  },
+
+  isReady() {
+    return Meteor.SubsCache.ready();
   },
 });
